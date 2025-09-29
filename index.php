13a32449246e834fa46bf3c41a061f21670a3d76
ellkoell <ellkoell@tsn.at>
2025-09-29 11:40:31 +0200

    Lokale css bootstrap dateien runtergeladen und eingebunden

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="de">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+<h1>Hello, world!</h1>
+</body>
+</html><?php
